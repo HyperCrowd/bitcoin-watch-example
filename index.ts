3282ca0109849c1d6e5512411a47ce2acdd6bec0
HyperCrowd <76200774+HyperCrowd@users.noreply.github.com>
2023-03-22 03:02:31 -0400

    3 files modified, 1 added

--- index.ts
+++ index.ts
@@ -3,11 +3,14 @@ import './style.css';
 import { render } from 'preact';
 import { html } from 'htm/preact';
 import { calculateStatistics, fetchTickerData, getPrices } from './src/main';
+import { getChart } from './src/report';
+import { Statistics } from './src/statistics';
+
+const stats: Statistics[] = [];
 
 async function update() {
   const ticker = await fetchTickerData();
   const prices = getPrices();
-  const stats = calculateStatistics(0.3);
 
   console.log(stats);
 
@@ -15,15 +18,141 @@ async function update() {
     return;
   }
 
+  const stat = calculateStatistics(0.3);
+  stats.push(stat);
+
   const body = html`
   <div>
+    <h1>Ask</h1>
+    <h2>Average</h2>
+    <div class="ask-average" />
 
+    <h2>Smoothness</h2>
+    <div class="ask-smoothness" />
+
+    <h2>Thresholds</h2>
+    <div class="ask-thresholds" />
+    
+    <h1>Bid</h1>
+    <h2>Average</h2>
+    <div class="bid-average" />
+
+    <h2>Smoothness</h2>
+    <div class="bid-smoothness" />
+
+    <h2>Thresholds</h2>
+    <div class="bid-thresholds" />
+
+    <h1>Last Trade Close</h1>
+    <h2>Average</h2>
+    <div class="lastTradeClose-average" />
+
+    <h2>Smoothness</h2>
+    <div class="lastTradeClose-smoothness" />
+
+    <h2>Thresholds</h2>
+    <div class="lastTradeClose-thresholds" />
+
+    <h1>High</h1>
+    <h2>Average</h2>
+    <div class="high-average" />
+
+    <h2>Smoothness</h2>
+    <div class="high-smoothness" />
+
+    <h2>Thresholds</h2>
+    <div class="high-thresholds" />
+
+    <h1>Low</h1>
+    <h2>Average</h2>
+    <div class="low-average" />
+
+    <h2>Smoothness</h2>
+    <div class="low-smoothness" />
+
+    <h2>Thresholds</h2>
+    <div class="low-thresholds" />
+
+    <h1>Opening</h1>
+    <h2>Average</h2>
+    <div class="opening-average" />
+
+    <h2>Smoothness</h2>
+    <div class="opening-smoothness" />
+
+    <h2>Thresholds</h2>
+    <div class="opening-thresholds" />
+
+    <h1>Trades</h1>
+    <h2>Average</h2>
+    <div class="trades-average" />
+
+    <h2>Smoothness</h2>
+    <div class="trades-smoothness" />
+
+    <h2>Thresholds</h2>
+    <div class="trades-thresholds" />
+
+    <h1>Volume</h1>
+    <h2>Average</h2>
+    <div class="volume-average" />
+
+    <h2>Smoothness</h2>
+    <div class="volume-smoothness" />
+
+    <h2>Thresholds</h2>
+    <div class="volume-thresholds" />
+
+    <h1>Volume (Weighted Average Price)</h1>
+    <h2>Average</h2>
+    <div class="volumeWeightedAveragePrice-average" />
+
+    <h2>Smoothness</h2>
+    <div class="volumeWeightedAveragePrice-smoothness" />
+
+    <h2>Thresholds</h2>
+    <div class="volumeWeightedAveragePrice-thresholds" />
   </div>
 `;
 
   render(body, document.body);
+
+  getChart('.ask-average', [], [
+    stats.map((stat) => stat.averages.high),
+    stats.map((stat) => stat.averages.ask),
+    stats.map((stat) => stat.averages.low)
+  ]);
+  getChart('.ask-smoothness', [], [[]]);
+  getChart('.ask-thresholds', [], [[]]);
+  getChart('.bid-average', [], [[]]);
+  getChart('.bid-smoothness', [], [[]]);
+  getChart('.bid-thresholds', [], [[]]);
+  getChart('.lastTradeClose-average', [], [[]]);
+  getChart('.lastTradeClose-smoothness', [], [[]]);
+  getChart('.lastTradeClose-thresholds', [], [[]]);
+  getChart('.high-average', [], [[]]);
+  getChart('.high-smoothness', [], [[]]);
+  getChart('.high-thresholds', [], [[]]);
+  getChart('.low-average', [], [[]]);
+  getChart('.low-smoothness', [], [[]]);
+  getChart('.low-thresholds', [], [[]]);
+  getChart('.opening-average', [], [[]]);
+  getChart('.opening-smoothness', [], [[]]);
+  getChart('.opening-thresholds', [], [[]]);
+  getChart('.trades-average', [], [[]]);
+  getChart('.trades-smoothness', [], [[]]);
+  getChart('.trades-thresholds', [], [[]]);
+  getChart('.volume-average', [], [[]]);
+  getChart('.volume-smoothness', [], [[]]);
+  getChart('.volume-thresholds', [], [[]]);
+  getChart('.volumeWeightedAveragePrice-average', [], [[]]);
+  getChart('.volumeWeightedAveragePrice-smoothness', [], [[]]);
+  getChart('.volumeWeightedAveragePrice-thresholds', [], [[]]);
 }
 
+/**
+ *
+ */
 async function main() {
   setInterval(await update, 10000);
   await update();
---
+++ src/report.ts
@@ -0,0 +1,26 @@
+import type {
+  Series,
+  SeriesObject,
+  SeriesPrimitiveValue,
+} from 'chartist/dist/core/types';
+import { LineChart } from 'chartist';
+
+export function getChart(
+  target: string,
+  labels: string[],
+  series: (Series<SeriesPrimitiveValue> | SeriesObject<SeriesPrimitiveValue>)[]
+) {
+  var data = {
+    labels,
+    series,
+  };
+
+  // As options we currently only set a static size of 300x200 px
+  var options = {
+    width: '300px',
+    height: '200px',
+  };
+
+  // In the global name space Chartist we call the Line function to initialize a line chart. As a first parameter we pass in a selector where we would like to get our chart created. Second parameter is the actual data object and as a third parameter we pass in our options
+  new LineChart(target, data, options);
+}
